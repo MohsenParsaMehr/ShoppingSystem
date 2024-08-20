6637c41756021aab290867ca37de0e89bee349e2
parsa <parsa@SHS-MPARSAMEHR>
2024-08-20 14:08:53 +0330

    charts added

---
+++ Controllers/ChartController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ChartJSCore.Models;
+using System.Collections.Generic;
+using ChartJSCore.Helpers;
+
+namespace SA51_CA_Project_Team10.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            Chart chart = new Chart();
+
+            chart.Type = Enums.ChartType.Line;
+
+            ChartJSCore.Models.Data data = new ChartJSCore.Models.Data();
+            data.Labels = new List<string>() { "January", "February", "March", "April", "May", "June", "July" };
+
+            LineDataset dataset = new LineDataset()
+            {
+                Label = "My First dataset",
+                Data = new List<double?> { 65, 59, 80, 81, 56, 55, 40 },
+                Fill = "false",
+                Tension = 0.1,
+                BackgroundColor = new List<ChartColor> { ChartColor.FromRgba(75, 192, 192, 0.4) },
+                BorderColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
+                BorderCapStyle = "butt",
+                BorderDash = new List<int> { },
+                BorderDashOffset = 0.0,
+                BorderJoinStyle = "miter",
+                PointBorderColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
+                PointBackgroundColor = new List<ChartColor> { ChartColor.FromHexString("#ffffff") },
+                PointBorderWidth = new List<int> { 1 },
+                PointHoverRadius = new List<int> { 5 },
+                PointHoverBackgroundColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
+                PointHoverBorderColor = new List<ChartColor> { ChartColor.FromRgb(220, 220, 220) },
+                PointHoverBorderWidth = new List<int> { 2 },
+                PointRadius = new List<int> { 1 },
+                PointHitRadius = new List<int> { 10 },
+                SpanGaps = false
+            };
+
+            data.Datasets = new List<Dataset>();
+            data.Datasets.Add(dataset);
+
+            chart.Data = data;
+
+            ViewData["chart"] = chart;
+
+            return View();
+        }
+    }
+}
